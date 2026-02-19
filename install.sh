8694c7a2d322a67d43a6e1824d63ef183401972f
uliboooo <uliboulibo@gmail.com>
2026-02-19 16:02:52 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,23 @@
 #/bin/bash
 
 echo "where do you want to install it?"
-read path
-echo "${path}"
+echo "\$HOME/.local/bin/lap_tap"
+echo -n "> "
+path="$HOME/.local/bin/lap_tap"
+read input_path
+if [ -z "$path" ]; then
+  path=${input_path}
+fi
 
+mkdir -p "${path}"
+echo "created install folder at ${path}"
 
+echo "start installing to ${path}"
+
+echo "Downloading resources from github ..."
+
+curl -L https://github.com/Uliboooo/lap_tap/releases/latest/download/lap_tap -o "${path}/lap_tap"
+curl -L https://github.com/Uliboooo/lap_tap/releases/latest/download/lap_tap_helper -p "${path}/lap_tap_helper"
+curl -L https://github.com/Uliboooo/lap_tap/releases/latest/download/resources -o "${path}/resouces"
+
+echo "please add ${path} to \$PATH"
